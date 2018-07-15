e9819360092fa75748515c6e8f263ee8f6174f2c
Minh Nhut <minhnhut37@gmail.com>
2018-07-15 13:41:53 +0900

    Remove test url

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const {DownloadWorker, utils} = require("./index");
 
 // Multi connections
-const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "movie.mkv", {
+const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "multi.file", {
     maxConnections: 8
 });
 worker.on('ready', () => {
@@ -14,7 +14,7 @@ worker.on('ready', () => {
 });
 
 // Single connection
-// const worker = new DownloadWorker("http://s57.linksvip.net/s/043/y0CXe39c6hAEXW8Qn1LizZ9PICy+l5s4j4lV7uk5uUrK3QO2Lt2qOyAIIX6DJLbAJK+yzZQrm9LbEYnK/super.troopers.2.2018.1080p.bluray.x264-drones.mkv", "movie.mkv", {
+// const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "single.file", {
 //     forceSingleConnection: true
 // });
 // worker.on('ready', () => {
