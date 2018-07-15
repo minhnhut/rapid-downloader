d90e79a206969fd6bd5f5a362a2b0cb6778871c4
Minh Nhut <minhnhut37@gmail.com>
2018-07-15 13:40:37 +0900

    Improve abstract session. Fix inconsistent newline character. Add git repo to package.json.

--- test.js
+++ test.js
@@ -1,19 +1,8 @@
 const {DownloadWorker, utils} = require("./index");
 
 // Multi connections
-// const worker = new DownloadWorker("http://ipv4.download.thinkbroadband.com/50MB.zip", "50.file");
-// worker.on('ready', () => {
-//     worker.on('start', () => console.log('started'))
-//     worker.on('progress', (progress) => {
-//         const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond);
-//         console.log(`${progress.percent}% - ${speed}`)
-//     });
-//     worker.start();
-// });
-
-// Single connection
-const worker = new DownloadWorker("http://ipv4.download.thinkbroadband.com/50MB.zip", "50.single", {
-    forceSingleConnection: true
+const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "movie.mkv", {
+    maxConnections: 8
 });
 worker.on('ready', () => {
     worker.on('start', () => console.log('started'))
@@ -23,3 +12,16 @@ worker.on('ready', () => {
     });
     worker.start();
 });
+
+// Single connection
+// const worker = new DownloadWorker("http://s57.linksvip.net/s/043/y0CXe39c6hAEXW8Qn1LizZ9PICy+l5s4j4lV7uk5uUrK3QO2Lt2qOyAIIX6DJLbAJK+yzZQrm9LbEYnK/super.troopers.2.2018.1080p.bluray.x264-drones.mkv", "movie.mkv", {
+//     forceSingleConnection: true
+// });
+// worker.on('ready', () => {
+//     worker.on('start', () => console.log('started'))
+//     worker.on('progress', (progress) => {
+//         const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond);
+//         console.log(`${progress.percent}% - ${speed}`)
+//     });
+//     worker.start();
+// });
