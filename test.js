bb81b2b489b8c3c78d94034b6847e8ced9e09759
Minh Nhut <minhnhut37@gmail.com>
2018-07-15 18:30:18 +0900

    - Implement spliting files, and rejoin files after done downloading - Improve slightly high CPU problem - Pump up version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const {DownloadWorker, utils} = require("./index");
 
 // Multi connections
-const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "multi.file", {
+const worker = new DownloadWorker("http://ipv4.download.thinkbroadband.com/50MB.zip", "50MB.zip", {
     maxConnections: 8
 });
 worker.on('ready', () => {
@@ -14,7 +14,7 @@ worker.on('ready', () => {
 });
 
 // Single connection
-// const worker = new DownloadWorker("http://mirror.filearena.net/pub/speed/SpeedTest_64MB.dat?_ga=2.217946004.329296576.1531622230-1523652149.1531622230", "single.file", {
+// const worker = new DownloadWorker("http://s28.linksvip.net/s/030/FnMjYR42ek7kHIatUl2ydQi2LJt3e8rGYTUmDVeNPLChmc2utvZm1Q2NxFzmmQ72OWjAStViyKZ9z3Nu/Rampage.2018.1080p.WEB-DL.DD5.1.H264-FGT.mkv", "single.file", {
 //     forceSingleConnection: true
 // });
 // worker.on('ready', () => {
