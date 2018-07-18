4f78d54eaf284a031468bef533a4ceef4d26d1cd
Minh Nhut <minhnhut37@gmail.com>
2018-07-18 18:09:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,32 @@
 const {DownloadWorker, utils} = require("./index");
 
 // Multi connections
-const worker = new DownloadWorker("http://ipv4.download.thinkbroadband.com/50MB.zip", "50MB.zip", {
+const worker = new DownloadWorker("http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin", "100MB-tokyo2.zip", {
     maxConnections: 8
 });
 worker.on('ready', () => {
     worker.on('start', () => console.log('started'))
     worker.on('progress', (progress) => {
-        const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond);
-        console.log(`${progress.percent}% - ${speed}`)
+        const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond, 2);
+        console.log(`${progress.completedPercent}% - ${speed}`)
     });
+    worker.on('finishing', () => console.log('Download is finishing'));
+    worker.on('end', () => console.log('Download is done'));
     worker.start();
 });
 
 // Single connection
-// const worker = new DownloadWorker("http://s28.linksvip.net/s/030/FnMjYR42ek7kHIatUl2ydQi2LJt3e8rGYTUmDVeNPLChmc2utvZm1Q2NxFzmmQ72OWjAStViyKZ9z3Nu/Rampage.2018.1080p.WEB-DL.DD5.1.H264-FGT.mkv", "single.file", {
+// const worker = new DownloadWorker("http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin", "100MB.zip", {
 //     forceSingleConnection: true
 // });
 // worker.on('ready', () => {
 //     worker.on('start', () => console.log('started'))
 //     worker.on('progress', (progress) => {
-//         const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond);
-//         console.log(`${progress.percent}% - ${speed}`)
+//         const speed = utils.dynamicSpeedUnitDisplay(progress.bytesPerSecond, 2);
+//         console.log(`${progress.completedPercent}% - ${speed}`)
 //     });
+//     worker.on('finishing', () => console.log('Download is finishing'));
+//     worker.on('end', () => console.log('Download is done'));
+//     worker.on('error', error => console.log(error));
 //     worker.start();
 // });
